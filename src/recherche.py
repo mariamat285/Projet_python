9f7ca0beb9d6e39b6c5885f0010cc7fa25e19ed5
mariamat285 <mariamat285@gmail.com>
2026-06-14 09:53:34 +0000

    changements

--- src/recherche.py
+++ src/recherche.py
@@ -2,7 +2,7 @@ def  rechercher_patient(patients_valides, critere, valeur):
     if critere in ["id", "nom", "prenom", "age", "telephone", "ville", "groupe_sanguin", "poids", "taille"]:
         resultat = []
         for patient in patients_valides:
-            if valeur.lower() in patient[critere].lower():
+            if valeur.lower() in str(patient[critere].lower()):
                 resultat.append(patient)
         return resultat
     else:
