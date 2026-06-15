e2dc6540d98f0dfcc379a6719d4ab3abe484180c
khadidiatou diasse <khadidiatoudiasse46@gmail.com>
2026-06-15 15:48:46 +0000

    fonction nettoyer_ville

--- src/nettoyage.py
+++ src/nettoyage.py
@@ -2,32 +2,48 @@
 def nettoyer_nom(nom):
     nom = nom.strip().title()
     return nom
+
 #-------------METTRE TOUT LES PRENOMS EN TITTLE----------#    
 def nettoyer_prenom(prenom):
     prenom = prenom.strip().title()
     return prenom
+
 #------------METTRE LES VILLE SOUS FORMAT CORRECT---------#
 def nettoyer_ville(ville):
-    corrections = {
-        "dakarr": "Dakar",
-        "dakkar": "Dakar",
-        "saint louis": "Saint-Louis",
-        "saint-louis": "Saint-Louis",  
-        "ziguincor": "Ziguinchor",
-        "ziginchor": "Ziguinchor",
-        "kaolak": "Kaolack",
-        "lougar": "Louga",
-        "tamba": "Tambacounda",
-        "diorbel": "Diourbel",
-        "thiès": "Thies",
-    }
+
+    villes = [
+        "Dakar", "Thiès", "Saint-Louis", "Kaolack", "Ziguinchor",
+        "Diourbel", "Fatick", "Kolda", "Louga", "Matam",
+        "Kaffrine", "Kédougou", "Sédhiou","Tambacounda"
+    ]
+
+    ville = ville.lower().strip()
+
+    meilleure_ville = ""
+    min_diff = 10
+
+    for v in villes:
+        v_test = v.lower().strip()
+
+        # différence de longueur
+        diff = abs(len(ville) - len(v_test))
+
+        # comparaison lettre par lettre
+        for i in range(min(len(ville), len(v_test))):
+            if ville[i] != v_test[i]:
+                diff += 1
+
+        # garder la meilleure ville
+        if diff < min_diff:
+            min_diff = diff
+            meilleure_ville = v
+
+    # vérification finale
+    if min_diff < len(meilleure_ville) / 2:
+        return meilleure_ville
+    else:
+        return "Ville inconnue"
     
-    ville = ville.strip().title()
-    
-    if ville.lower() in corrections:
-        ville = corrections[ville.lower()]
-    
-    return ville
 #-------------NETTOYER TELEPHONE-------------#
 def nettoyer_telephone(telephone):
     
@@ -39,15 +55,19 @@ def nettoyer_telephone(telephone):
         telephone = telephone[5:]
     
     return telephone
+
 #-------------NETTOYER AGE-------------#
 def nettoyer_age(age):
     return age.strip()
+
 #-------------NETTOYER POIDS-------------#
 def nettoyer_poids(poids):
     return poids.strip()
+
 #-------------NETTOYER TAILLE-------------#
 def nettoyer_taille(taille):
     return taille.strip()
+
 #-------------NETTOYER DOUBLONS-------------#
 def nettoyer_doublons(patients_valides):
     liste_propre = []
